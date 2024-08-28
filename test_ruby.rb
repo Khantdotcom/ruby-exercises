883a5519343cd15edc975c504224aa4c74cbd92f
Khantdotcom <kokhanthtay@gmail.com>
2024-08-28 21:16:11 +0630

    done

--- test_ruby.rb
+++ test_ruby.rb
@@ -1,14 +1,14 @@
-require binding.pry
 def casesar_cipher(string, count)
-  
   #lowercase >> split >> locate index >> change index if sum > 25 sum // 25 -1>> locate letter >> cap
   array = ('a'..'z').to_a
-  string.lowercase!.each |x| do
-    binding.pry
-    new_index = array.find_index(x) + count
-    if new_index > 25 
-      new_index // 25 -1
-    x = array[sum]
-  string.capitalize
-
-casesar_cipher("Hgjdkfjd jdkjfd", 29)
+  down_string = string.downcase.chars.map do |char|
+    if array.include?(char)
+      new_index = (array.index(char) + count) % 26
+      array[new_index]
+    else
+      char
+    end
+  end
+  result = down_string.join.capitalize
+  result
+  end
